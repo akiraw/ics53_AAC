6ad4454d78c1139eade14ddf358dfd4e8d7e43e9
akiraw <wong.akira95@gmail.com>
2016-04-19 13:16:48 -0700

    april 19
    parse

--- Lab2/Lab2.c
+++ Lab2/Lab2.c
@@ -2,33 +2,62 @@
 #include <string.h>
 #include <stdbool.h>
 
+void getUserInput(char * uiBuffer);
+
 int main() {
 	char uInputBuffer[2048];
 	bool ctrloop = true;
 	
 	//Outer Control Loop
 	while ( ctrloop ) {
-	  
-	  bool uiloop = true;
-	  printf("prompt> ");
-	  //Get User Input
-	  int index = 0;
-	  while (uiloop) {
+		getUserInput(uInputBuffer);
+		printf("You entered: \n %s \n", uInputBuffer);
+		// parser 
+		bool interpretLoop = true;
+		int index = 0;
+		
+		while (interpretLoop) {
+			bool parseLoop = true;
+			int tInfo = 0;
+			char tokenBuffer[255];
+		
+			while(parseLoop) {
+				int c = uInputBuffer[index++];
+				
+				if (c == '\0') {
+						parseLoop = false;
+						interpretLoop = false;
+				}
+				else if (c != ' ') 
+						tokenBuffer[tInfo++] = c; 
+				else if (tInfo != 0)
+						parseLoop = false;
+				//just loops if there is a leading space
+			}
+			tokenBuffer[tInfo] = '\0';
+		
+			printf("\n token: %s \n", tokenBuffer);
+		
+			if ( strcmp("quit", tokenBuffer) == 0)
+				interpretLoop = false;
+				ctrloop = false;
+		}
+	}
+	return 0;
+}
+
+void getUserInput(char * uiBuffer) {
+	bool uiloop = true;
+	printf("prompt> ");
+	//Get User Input
+	int index = 0;
+	while (uiloop) {
 		int c = getchar();
 
 		if ( c == EOF || c == '\n') {
-			uInputBuffer[index] = '\0';
+			uiBuffer[index] = '\0';
 			uiloop = false;
-		}	else {
-		  //TODO: Elimiate Whitespace & blank lines
-			uInputBuffer[index++] = c;
-		}
-	  }
-
-	  printf("You entered: \n %s \n", uInputBuffer);
-	
-	  if ( strcmp("quit", uInputBuffer) == 0)
-	    ctrloop = false;
-	  }
-	return 0;
+		}	else 
+			uiBuffer[index++] = c;
+	}
 }
