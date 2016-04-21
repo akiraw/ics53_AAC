0efde9776472a839ebd3fe0c882ee12977746b45
akiraw <wong.akira95@gmail.com>
2016-04-21 13:00:10 -0700

    April 21
    changes made to token array

--- Lab2/Lab2.c
+++ Lab2/Lab2.c
@@ -3,45 +3,37 @@
 #include <stdbool.h>
 
 void getUserInput(char * uiBuffer);
+int parseUserInput(char tokenArray[][256], const char * uiBuffer);
+void printTokens( char tokenArray[][256], int numTokens);
 
 int main() {
-	char uInputBuffer[2048];
+	char uiBuffer[2048];
 	bool ctrloop = true;
 	
 	//Outer Control Loop
 	while ( ctrloop ) {
-		getUserInput(uInputBuffer);
-		printf("You entered: \n %s \n", uInputBuffer);
-		// parser 
-		bool interpretLoop = true;
-		int index = 0;
+	
+		//Prompt and enter user input
+		getUserInput(uiBuffer);
+		//print statement for debugging
+		printf("You entered: \n %s \n", uiBuffer);
 		
-		while (interpretLoop) {
-			bool parseLoop = true;
-			int tInfo = 0;
-			char tokenBuffer[255];
+		// parsing user input into array of c strings
+		char tokenArray[128][256];
+		int count = parseUserInput(tokenArray, uiBuffer);
+		//print token array for debugging
+		printTokens(tokenArray, count);
 		
-			while(parseLoop) {
-				int c = uInputBuffer[index++];
-				
-				if (c == '\0') {
-						parseLoop = false;
-						interpretLoop = false;
-				}
-				else if (c != ' ') 
-						tokenBuffer[tInfo++] = c; 
-				else if (tInfo != 0)
-						parseLoop = false;
-				//just loops if there is a leading space
-			}
-			tokenBuffer[tInfo] = '\0';
-		
-			printf("\n token: %s \n", tokenBuffer);
-		
+		//bool interpretLoop = true;
+		//while (interpretLoop) {
+			
+		//interpretLoop = false;
+			/*
 			if ( strcmp("quit", tokenBuffer) == 0)
 				interpretLoop = false;
 				ctrloop = false;
-		}
+			*/
+		//}
 	}
 	return 0;
 }
@@ -60,4 +52,36 @@ void getUserInput(char * uiBuffer) {
 		}	else 
 			uiBuffer[index++] = c;
 	}
+}
+
+int parseUserInput(char tokenArray[][256], const char * uiBuffer)
+{
+	int tokenCounter = 0;
+	int UIIndex = 0;
+	bool parseCrtlLoop = true;
+	while(parseCrtlLoop) {
+		int tIndex = 0;
+		char tokenBuffer[256];
+		bool parseLoop = true;
+		while(parseLoop) {
+			int c = uiBuffer[UIIndex++];
+			if (c == '\0') {
+				parseLoop = false;
+				parseCrtlLoop = false;
+			} else if (c != ' ') {
+				tokenBuffer[tIndex++] = c; 
+			} else if (tIndex != 0) {
+				parseLoop = false;
+			}
+			//just loops if there is a leading space
+		}
+		tokenBuffer[tIndex] = '\0';
+		strcpy(tokenArray[tokenCounter++], tokenBuffer);
+	}
+	return tokenCounter;
+}
+
+void printTokens(char tokenArray[][256], int numTokens) {
+	for ( int i = 0 ; i < numTokens; ++i )
+		printf("token at index  %d: %s \n", i, tokenArray[i]);
 }
